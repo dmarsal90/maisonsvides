f1c2d175a9963c824857332bd6f9c90aa176defb
David Marsal Ivars <david@flexvision.be>
2023-03-27 00:02:15 -0400

    Remove User Import

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Passport\HasApiTokens;
-use App\Models\User;
+
 
 class User extends Authenticatable
 {
