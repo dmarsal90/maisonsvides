5291ad6c8ae5b4fc9fd333411b876f80661a9c5e
David Marsal Ivars <david@flexvision.be>
2023-06-02 18:51:41 -0400

    Update sendSmsReminder function

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -16,15 +16,29 @@ class SmsController extends Controller
 
     public function sendSmsReminder(Request $request)
     {
-        $to = $request->input('to');
-        $message = $request->input('message');
+        $to = '+' . $request->input('to');
+        $message = $request->input('body');
 
-        $result = $this->lwsSmsService->sendSms($to, $message);
+        // Reemplazar los marcadores de posición en el mensaje con los valores correspondientes
+        $message = str_replace('{seller_name}', $request->input('seller_name'), $message);
+        $message = str_replace('{estate_address}', $request->input('estate_address'), $message);
+        $message = str_replace('{modal_date}', $request->input('modal_date'), $message);
+        $message = str_replace('{modal_date_confirm_start}', $request->input('modal_date_confirm_start'), $message);
+        $message = str_replace('{modal_date_confirm_end}', $request->input('modal_date_confirm_end'), $message);
 
-        if ($result) {
-            return response()->json(['status' => 'success']);
-        } else {
-            return response()->json(['status' => 'error']);
+        // Eliminar las etiquetas HTML del mensaje
+        $message = strip_tags($message);
+
+        try {
+            $result = $this->lwsSmsService->sendSms($to, $message);
+
+            if ($result) {
+                return response()->json(['status' => 'success']);
+            } else {
+                return response()->json(['status' => 'error', 'message' => 'Error sending SMS']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()]);
         }
     }
 }
