bfe1c21a45dff282f940731634e483fade46121e
David Marsal Ivars <david@flexvision.be>
2023-06-02 16:22:56 -0400

    Add SmsController

---
+++ app/Http/Controllers/SmsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\LwsSmsService;
+
+class SmsController extends Controller
+{
+    protected $lwsSmsService;
+
+    public function __construct(LwsSmsService $lwsSmsService)
+    {
+        $this->lwsSmsService = $lwsSmsService;
+    }
+
+    public function sendSms(Request $request)
+    {
+        $to = $request->input('to');
+        $message = $request->input('message');
+
+        $result = $this->lwsSmsService->sendSms($to, $message);
+
+        if ($result) {
+            return response()->json(['status' => 'success']);
+        } else {
+            return response()->json(['status' => 'error']);
+        }
+    }
+}
