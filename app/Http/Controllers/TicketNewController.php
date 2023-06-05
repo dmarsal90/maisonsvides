9c0f78fcddbc717c5b665f19c7a8f4bef7a52d20
David Marsal Ivars <david@flexvision.be>
2023-06-05 08:21:25 -0400

    Add createTicket function

--- app/Http/Controllers/TicketNewController.php
+++ app/Http/Controllers/TicketNewController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Ticket;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewTicketNotification;
 
 class TicketNewController extends Controller
 {
@@ -53,5 +55,40 @@ class TicketNewController extends Controller
         ]);
 
         return redirect()->back()->with('success', 'Le ticket a été créé avec succès.');
+    }
+
+    public function createTicket(Request $request)
+    {
+        // Save all data of request
+        $data = $request->all();
+        try {
+            // Check if ticket already exists
+            $existingTicket = Ticket::where('name', $data['name_ticket'])
+                ->where('email', $data['email'])
+                ->where('subject', $data['subject'])
+                ->where('comment', $data['body'])
+                ->first();
+            if ($existingTicket) {
+                return back()->with('error', 'Le ticket existe déjà.');
+            }
+
+            // Save new ticket
+            $ticket = Ticket::create([
+                'name' => $data['name_ticket'],
+                'email' => $data['email'],
+                'subject' => $data['subject'],
+                'comment' => $data['body'],
+                'isSolved'=> 0,
+                'agent_id'=> 2,
+            ]);
+            
+
+            // Send email notification
+            Mail::to('david@flexvision.be')->send(new NewTicketNotification());
+
+            return back()->with('success', 'Le ticket a été créé avec succès.');
+        } catch (\Exception $e) {
+            return back()->with('error', 'Le ticket n\'a pas pu être créé. Erreur: ' . $e->getMessage());
+        }
     }
 }
