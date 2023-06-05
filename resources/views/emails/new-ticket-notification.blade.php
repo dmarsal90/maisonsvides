57be8365e7892b45680f824e203f4da3a4bc5dee
David Marsal Ivars <david@flexvision.be>
2023-06-05 13:36:38 -0400

    Update new-ticket-notification view

--- resources/views/emails/new-ticket-notification.blade.php
+++ resources/views/emails/new-ticket-notification.blade.php
@@ -8,11 +8,19 @@
 
 <body>
     <h1>Nouveau ticket créé</h1>
-    @if (is_string($message))
-    <p>{{ htmlspecialchars($message) }}</p>
-    @else
-    <p>Le message du ticket est vide.</p>
-    @endif
+    <p>Un nouveau ticket a été créé:</p>
+    <ul>
+        <li>Nom: {{ $ticket['name'] }}</li>
+        <li>Email: {{ $ticket['email'] }}</li>
+        <li>Sujet: {{ $ticket['subject'] }}</li>
+        <li>Commentaire: {{ $ticket['comment'] }}</li>
+        <li>Résolu: {{ $ticket['isSolved'] ? 'oui' : 'non' }}</li>
+        @if ($ticket['agent_id'] == null)
+            <li>Merci de bien vouloir assigner un agent à ce ticket.</li>
+        @else
+            <li>ID agent: {{ $ticket['agent_id'] }}</li>
+        @endif
+    </ul>
 </body>
 
 </html>
